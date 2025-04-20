4c8e542f2a668dbe987abffc5c3f791af23e2c65
matheusraffu <matheus.martinez@sptech.school>
2025-04-19 23:38:57 -0300

    Hamburguer Icon

---
+++ web-data-viz/public/js/index.js
@@ -0,0 +1,7 @@
+function abrirMenu() {
+    document.getElementById('menuFull').classList.add('ativo');
+  }
+
+  function fecharMenu() {
+    document.getElementById('menuFull').classList.remove('ativo');
+  }
