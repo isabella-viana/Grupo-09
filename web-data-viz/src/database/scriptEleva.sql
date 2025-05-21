baf3d5ee100face0020b4fc169f0318b00b7ff10
Victor-Hugo1 <victor.hsouza@sptech.school>
2025-05-20 23:43:01 -0300

    update bd

--- web-data-viz/src/database/scriptEleva.sql
+++ web-data-viz/src/database/scriptEleva.sql
@@ -6,28 +6,26 @@ CREATE TABLE empresa (
     nome VARCHAR(45),
     email VARCHAR(45),
     cnpj CHAR(14),
-    senha VARCHAR(20),    
-    dtCadastro DATE
+    senha VARCHAR(20)
 );
 
 CREATE TABLE endereco (
     idendereco INT PRIMARY KEY,
     rua VARCHAR(45),
+    numero INT(45),
     bairro VARCHAR(45),
     cidade VARCHAR(45),
-    uf VARCHAR(45),
     cep VARCHAR(45),
     empresa_idempresa INT,
     FOREIGN KEY (empresa_idempresa) REFERENCES empresa(idempresa)
 );
 
-CREATE TABLE colaborador (
-    idcolaborador INT PRIMARY KEY,
+CREATE TABLE funcionario (
+    idfuncionario INT PRIMARY KEY,
     cpf CHAR(8),
     email VARCHAR(45),
     senha VARCHAR(45),
     cargo VARCHAR(45),
-    colaboradorcol VARCHAR(45),
     empresa_idempresa INT,
     FOREIGN KEY (empresa_idempresa) REFERENCES empresa(idempresa)
 );
@@ -42,21 +40,18 @@ CREATE TABLE consumo_energia (
     regiao VARCHAR(45)
 );
 
-CREATE TABLE avisos (
+CREATE TABLE avisos_slack (
     idavisos INT PRIMARY KEY,
     descricao VARCHAR(45),
     dtHoraEmissao DATE
 );
 
-CREATE TABLE logs_service (
-    idregistroLogs INT PRIMARY KEY auto_increment,
-    nomeArquivo VARCHAR(100),
-    inicio DATETIME,
-    fim DATETIME,
-    logs_registrados INT,
-    erros INT,
-    sucesso VARCHAR(45),
-    mensagem VARCHAR(45)
+CREATE TABLE log_eleva (
+    id INT primary key auto_increment,
+    dataHora TIMESTAMP,
+    nivel VARCHAR(10),
+    classe VARCHAR(100),
+    mensagem TEXT
 );
 
 
