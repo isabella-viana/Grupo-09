2461082cc90265d9df4961474f1f8ee1ffe1bfb5
Vingonc <vinicius.gcosta@sptech.school>
2025-05-31 20:32:23 -0300

    Edição da funcionalidade de direcionadores da barra lateral

---
+++ web-data-viz/public/js/direcionadores.js
@@ -0,0 +1,18 @@
+function sair() {
+  window.location.href = "./index.html";
+}
+function irParaDashboard() {
+  window.location.href = "./dashboard.html";
+}
+function irParaEnderecos() {
+  window.location.href = "./";
+}
+function irParaGerenciar() {
+  window.location.href = "./gerenciamento.html";
+}
+function irParaConta() {
+  window.location.href = "./conta_usuario.html";
+}
+function irParaSolicitacao() {
+  window.location.href = "./solicitacao.html";
+}
