069188104b870d2587ea1d7c884e482c8038ae07
PGandin <pedro.gandin@sptech.school>
2025-06-09 16:50:50 -0300

    final banco

--- web-data-viz/src/database/scriptBD.sql
+++ web-data-viz/src/database/scriptBD.sql
@@ -48,7 +48,7 @@ CREATE TABLE endereco (
     empresa_idempresa INT,
     usuario_idUsuario INT,
     FOREIGN KEY (empresa_idempresa) REFERENCES empresa(idempresa) on delete cascade,
-    FOREIGN KEY (usuario_idUsuario) REFERENCES Usuario(idUsuario) on delete cascade
+    FOREIGN KEY (usuario_idUsuario) REFERENCES usuario(idUsuario) on delete cascade
 );
 
 CREATE TABLE leads (
